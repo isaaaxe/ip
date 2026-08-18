7177f726c0ba8b4920a711a558bc0fb0950b8973
isaaaxe <isaacchoong1901@gmail.com>
2026-08-18 14:50:37 +0800

    Level 0 increment: Rename and update Chatbot file

---
+++ src/main/java/Apollo.java
@@ -0,0 +1,10 @@
+public class Apollo {
+    public static void main(String[] args) {
+        String greeting = "Greetings young mortal! Apollo here to answer any queries under the sun!";
+        String exit = "To the end of the west wind, where fresh flowers bloom.";
+        System.out.println("=======================================================================");
+        System.out.println(greeting);
+        System.out.println("=======================================================================");
+        System.out.println(exit);
+    }
+}
