999560a5bfa4d897654b9b086c8dc2916086e9c9
isaaaxe <isaacchoong1901@gmail.com>
2026-08-18 16:45:13 +0800

    Add completion marking/unmarking ability

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -4,7 +4,7 @@ public class Apollo {
     final static String greeting = "Greetings young mortal! Apollo here to answer any queries under the sun!";
     final static String exit = "To the end of the west wind, where fresh flowers bloom.";
     static boolean validSession = true;
-    static String[] record  = new String[100];
+    static Task[] record  = new Task[100];
     static Scanner scanner;
     static int listCount = 0;
 
@@ -17,18 +17,41 @@ public class Apollo {
         System.out.println(greeting);
         while (validSession) {
             printBarrier();
-            String command = scanner.nextLine();
+            String fullCommand = scanner.nextLine();
+            String[] commandArgs = fullCommand.trim().split(" ");
             //command switch case
-            if (command.equals("bye")) {
+            if (commandArgs[0].equals("bye")) {
                 validSession = false;
-            } else if (command.equals("list")) {
+            } else if (commandArgs[0].equals("list")) {
                 for (int i = 0; i < listCount; i++) {
-                    System.out.println(String.format("%d. %s", i + 1, record[i]));
+                    System.out.print(String.format("%d. ", i+1));
+                    printTask(record[i]);
+                }
+            } else if (commandArgs[0].equals("mark")) {
+                try {
+                    int index = Integer.parseInt(commandArgs[1]) - 1;
+                    Task task = record[index];
+                    task.markAsDone();
+                    printMarkChanges(task.isDone);
+                    printTask(task);
+                } catch (Error e) {
+                    printErrorMessage();
+                }
+                 
+            } else if (commandArgs[0].equals("unmark")) { 
+                try {
+                    int index = Integer.parseInt(commandArgs[1]) - 1;
+                    Task task = record[index];
+                    task.markAsUndone();     
+                    printMarkChanges(task.isDone);
+                    printTask(task);
+                } catch (Error e) {
+                    printErrorMessage();
                 }
             } else {
-                record[listCount] = command;
+                record[listCount] = new Task(fullCommand);
                 listCount += 1;
-                printCommand(command);
+                printCommand(fullCommand);
             }
 
         }
@@ -44,4 +67,16 @@ public class Apollo {
     public static void printCommand(String command) {
         System.out.println("    added: " + command);
     }
+
+    public static void printTask(Task task) {
+        System.out.println(String.format("[%s] %s",task.getStatusIcon(), task.description));
+    }
+
+    public static void printErrorMessage() {
+        System.out.println("Invalid command passed, please try again.");
+    }
+
+    public static void printMarkChanges(boolean isMarked) {
+        System.out.println(String.format("Your prayers are heard child, I have %s the specified task:", isMarked ? "marked" : "unmarked"));
+    } 
 }
