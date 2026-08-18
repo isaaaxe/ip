8434667beaada01ed592d9c58cf971a5762cf370
isaaaxe <isaacchoong1901@gmail.com>
2026-08-18 15:09:14 +0800

    Added echo and exit logic

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Apollo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = "Greetings young mortal! Apollo here to answer any queries under the sun!";
         String exit = "To the end of the west wind, where fresh flowers bloom.";
-        System.out.println("=======================================================================");
+        boolean validSession = true;
+        printBarrier();
         System.out.println(greeting);
-        System.out.println("=======================================================================");
+        while (validSession) {
+            printBarrier();
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                validSession = false;
+            } else {
+                printCommand(command);
+            }
+        }
+        printBarrier();
+
         System.out.println(exit);
+        scanner.close();
+    }
+
+    public static void printBarrier() {
+        System.out.println("=======================================================================");
+    } 
+    public static void printCommand(String command) {
+        System.out.println("    " + command);
     }
 }
