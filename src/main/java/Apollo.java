3baa59bba576b1c56b02b4095b2f306d9f701b87
isaaaxe <isaacchoong1901@gmail.com>
2026-08-20 09:59:35 +0800

    Handle errors/exceptions

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -31,89 +31,83 @@ public class Apollo {
             String[] inputArgs = input.trim().split(" ");
             printSeparator();
 
-            Command command = Command.valueOf(inputArgs[0].trim().toUpperCase());
+            
             //command switch case
-            switch (command) {
-                case BYE:
-                    validSession = false;
-                    break;
-                case LIST:
-                    printList();
-                    break;
-                case MARK:
-                    int indexMark = Integer.parseInt(inputArgs[1]) - 1;
-                    markAsDone(indexMark);
-                    break;
-                case UNMARK:
-                    int indexUnmark = Integer.parseInt(inputArgs[1]) - 1;
-                    markAsUndone(indexUnmark);
-                    break;
-                case TODO:
-                    String todoDescription = input.substring("todo".length()).trim();
-                    Todo todo = new Todo(todoDescription);
-                    addTask(todo);
-                    break;
-                case EVENT:
-                    String eventArgs = input.substring("event".length()).trim();
-                    int fromIndex = eventArgs.indexOf("/from");
-                    int toIndex = eventArgs.indexOf("/to");
+            try {
+                Command command = Command.valueOf(inputArgs[0].trim().toUpperCase());
+                switch (command) {
+                    case BYE:
+                        validSession = false;
+                        break;
+                    case LIST:
+                        printList();
+                        break;
+                    case MARK:
+                        try {
+                            int indexMark = Integer.parseInt(inputArgs[1]) - 1;
+                            markAsDone(indexMark);
+                        } catch (Exception e) {
+                            System.out.println("Give a valid index mortal");
+                        } 
+                        break;
+                        
+                    case UNMARK:
+                        try {
+                            int indexUnmark = Integer.parseInt(inputArgs[1]) - 1;
+                            markAsUndone(indexUnmark);
+                        } catch (Exception e) {
+                            System.out.println("Give a valid index mortal");
+                        }
+                        break;
+                    case TODO:
+                        try {
+                            String todoDescription = input.substring("todo".length()).trim();
+                            if (todoDescription.trim().length() < 1) {
+                                throw new Exception("Invalid arguments");
+                            }
+                            Todo todo = new Todo(todoDescription);
+                            addTask(todo);
+                        } catch (Exception e) {
+                            System.out.println("The description cannot be empty mortal!");
+                        }
+                        break;
+                    case EVENT:
+                        try {
+                            String eventArgs = input.substring("event".length()).trim();
+                            int fromIndex = eventArgs.indexOf("/from");
+                            int toIndex = eventArgs.indexOf("/to");
 
-                    String eventDescription = eventArgs.substring(0, fromIndex);
-                    String fromString = eventArgs.substring(fromIndex + "/from".length(), toIndex);
-                    String toString = eventArgs.substring(toIndex + "/to".length());
-                    Event event = new Event(eventDescription, fromString, toString);
-                    addTask(event);
+                            String eventDescription = eventArgs.substring(0, fromIndex);
+                            String fromString = eventArgs.substring(fromIndex + "/from".length(), toIndex);
+                            String toString = eventArgs.substring(toIndex + "/to".length());
+                            Event event = new Event(eventDescription, fromString, toString);
+                            addTask(event);
+                        } catch (Exception e) {
+                            System.out.println("Give me good arguments mortal!");
+                        }
+                        break;
+                    case DEADLINE:
+                        try {
+                            String deadlineArgs = input.substring("deadline".length()).trim();
+                            int deadlineIndex = deadlineArgs.indexOf("/by");
 
-                    break;
-                case DEADLINE:
-                    String deadlineArgs = input.substring("deadline".length()).trim();
-                    int deadlineIndex = deadlineArgs.indexOf("/by");
-
-                    String deadlineDescription = deadlineArgs.substring(0, deadlineIndex);
-                    String deadlineString = deadlineArgs.substring(deadlineIndex + "/by".length());
-                    Deadline deadline  = new Deadline(deadlineDescription, deadlineString);
-                    addTask(deadline);
-                    break;
-                case DELETE:
-                    //not implemented yet
-                    break;
+                            String deadlineDescription = deadlineArgs.substring(0, deadlineIndex);
+                            String deadlineString = deadlineArgs.substring(deadlineIndex + "/by".length());
+                            Deadline deadline  = new Deadline(deadlineDescription, deadlineString);
+                            addTask(deadline);
+                        } catch (Exception e) {
+                            System.out.println("Give me good arguments mortal!");
+                        }
+                        break;
+                    case DELETE:
+                        //not implemented yet
+                        break;
             }
+        } catch (Exception e) {
+            printErrorMessage();
+        } finally { 
             printSeparator();
-
-            // if (commandArgs[0].equals("bye")) {
-            //     validSession = false;
-            // } else if (commandArgs[0].equals("list")) {
-            //     for (int i = 0; i < listCount; i++) {
-            //         System.out.print(String.format("%d. ", i+1));
-            //         printTask(record[i]);
-            //     }
-            // } else if (commandArgs[0].equals("mark")) {
-            //     try {
-            //         int index = Integer.parseInt(commandArgs[1]) - 1;
-            //         Task task = record[index];
-            //         task.markAsDone();
-            //         printMarkChanges(task.isDone);
-            //         printTask(task);
-            //     } catch (Exception e) {
-            //         printErrorMessage();
-            //     }
-                 
-            // } else if (commandArgs[0].equals("unmark")) { 
-            //     try {
-            //         int index = Integer.parseInt(commandArgs[1]) - 1;
-            //         Task task = record[index];
-            //         task.markAsUndone();     
-            //         printMarkChanges(task.isDone);
-            //         printTask(task);
-            //     } catch (Exception e) {
-            //         printErrorMessage();
-            //     }
-            // } else {
-            //     record[listCount] = new Task(fullCommand);
-            //     listCount += 1;
-            //     printCommand(fullCommand);
-            // }
-
+            }
         }
         System.out.println(exit);
         printBarrier();
@@ -129,7 +123,7 @@ public class Apollo {
     }
 
     public static void printErrorMessage() {
-        System.out.println("Invalid command passed, please try again.");
+        System.out.println("Invalid command given, try again mortal.");
     }
 
     public static void printMarkChanges(Task task) {
@@ -146,6 +140,7 @@ public class Apollo {
     
     //list: directly prints the wanted output
     public static void printList() {
+        System.out.println("Here are your current tasks child: ");
         for (int i = 0; i < listCount; i++) {
             System.out.println(String.format("%d. %s", i+1, record[i]));
         }
